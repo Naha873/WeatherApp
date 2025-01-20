aa44d9ff652259394e78959240242971e4428e27
Hana Wainwright <hana_wainwright@Hanas-MacBook-Pro.local>
2025-01-20 20:21:22 +0000

    Creating a function to convert celsius to farenheit using tdd

--- index.js
+++ index.js
@@ -66,5 +66,13 @@ async function getWeatherData(latitude, longitude) {
       throw error; 
     }
   }
- module.exports = {getWeatherData, getDailyWeatherData, getLocation}; // Export the function directly
+
+  function celsiusToFahrenheit(celsius) {
+    if (typeof celsius !== 'number') {
+      throw new Error('Input must be a number'); 
+    }
+    return (celsius * 9/5) + 32;
+  }
+  
+ module.exports = {getWeatherData, getDailyWeatherData, getLocation, celsiusToFahrenheit}; // Export the function directly
   
